140ca13596fd62c6d446c5c0a3ad9925ed97dc12
danielk-harper <d_kaeseberg1@mail.harpercollege.edu>
2024-09-01 23:22:31 -0500

    Fixed code to comply with acceptance criteria.

--- src/main/java/com/csc/Menu.java
+++ src/main/java/com/csc/Menu.java
@@ -8,7 +8,7 @@ public class Menu {
   {
     int i;
     
-    i = promptInteger(10, 20, "Give integer between 10 and 20 inclusive.", "Bad number! Try again.");
+    i = promptInteger(true, 10, true, 20, "Give integer between 10 and 20 inclusive.", "Bad number! Try again.");
     
     if (i > 20 || i < 10) {
       System.out.println(String.format("Bad number got through! Number: %d", i));
@@ -16,27 +16,48 @@ public class Menu {
       System.out.println(String.format("Number provided: %d", i));
     }
     
-    i = promptInteger(-20, -10, "Give integer between -10 and -20 inclusive.", "Bad number! Try again.");
+    i = promptInteger(true, -20, true, -10, "Give integer between -10 and -20 inclusive.", "Bad number! Try again.");
     
     if (i > -10 || i < -20) {
       System.out.println(String.format("Bad number got through! Number: %d", i));
     } else {
       System.out.println(String.format("Number provided: %d", i));
     }
-  }
-  
-  public static int promptInteger(int lower_bound, int upper_bound, String question_prompt, String error_prompt) {
-    Scanner in = new Scanner(System.in);
     
-    System.out.println(question_prompt);
-    int value = in.nextInt();
-    while (value < lower_bound || value > upper_bound) {
-      System.out.println(error_prompt);
-      System.out.println("");
-      System.out.println(question_prompt);
-      value = in.nextInt();
+    i = promptInteger(false, 0, true, -10, "Give integer less than -10.", "Bad number! Try again.");
+    
+    if (i > -10) {
+      System.out.println(String.format("Bad number got through! Number: %d", i));
+    } else {
+      System.out.println(String.format("Number provided: %d", i));
     }
     
-    return value;
+    i = promptInteger(false, 0, false, 0, "Give any integer.", "Bad integer.");
+    
+    System.out.println(String.format("Number provided: %d", i));
+  }
+  
+  private static boolean is_value_between_or_at_bounds(int lower_bound, int upper_bound, int value) {
+    return ((lower_bound <= value) && (upper_bound >= value));
+  }
+  
+  public static int promptInteger(boolean use_lower_bound, int lower_bound, boolean use_upper_bound, int upper_bound, String question_prompt, String error_prompt) {
+    Scanner in = new Scanner(System.in);
+    
+    while (true) {
+      System.out.println(question_prompt);
+      int value = in.nextInt();
+      
+      // these checks will ensure that if we aren't supposed to use the bounds, the bounds will always succeed when compared against.
+      if (!use_lower_bound) lower_bound = value;
+      if (!use_upper_bound) upper_bound = value;
+      
+      if (Menu.is_value_between_or_at_bounds(lower_bound, upper_bound, value)) {
+        return value;
+      }
+      
+      System.out.println(error_prompt);
+      System.out.println("");
+    }
   }
 }
